725bd8fee095634c60d0895d7e2d2fb50bd53e62
CranSauce <atlankinlogs@gmail.com>
2024-10-23 17:18:23 -0400

    CrashTest

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ mongoose
 
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 app.use("/", mainRouter);
 
 app.listen(PORT, '0.0.0.0', () => {});
